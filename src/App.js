59855570a8e6bf72c336c85c8f659a789bf73159
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-12-22 14:12:57 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import CounterButton from './components/CounterButton';
 
 function App() {
   return (
     <div className="App">
       <Welcome/>
+      <CounterButton/>
     </div>
   );
 }
--- timer.js
+++ timer.js
@@ -1,1 +1,2 @@
-3/26
+5/26
+https://www.youtube.com/watch?v=ePpKIIqHt6I&list=PLNkWIWHIRwME_Gv2vlWAR6TfeSXylYfw4&index=5&ab_channel=webDev
